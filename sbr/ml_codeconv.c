ae050c6d878654195d5d844df3df37422483b5cf
Shinji KONO <kono@ie.u-ryukyu.ac.jp>
2014-12-31 21:40:38 +0900

    add body decode in mhlsbr

---
+++ sbr/ml_codeconv.c
@@ -0,0 +1,190 @@
+/* ml_codeconv.c - (multilingual) code conversion */
+/*			by takada@seraph.ntt.jp   */
+/*			arranged by MH-plus project */
+
+#ifdef JAPAN
+
+#include "../h/mh.h"
+#include "../h/mhparse.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+static void ml_conv_sbr();
+
+/* NKF Input/Output pointers */
+
+static unsigned char *nkf_output;
+static unsigned char *nkf_input;
+static int nkf_input_ctr;
+static int nkf_output_ctr;
+static int nkf_limit;
+static int nkf_ilimit;
+static void (*nkf_flush)(unsigned char *,int);
+
+static void nkf_open(unsigned char *opt, unsigned char *in,int ilimit,
+	unsigned char *out,int limit,void (*flush)(unsigned char *,int),FILE *fp);
+static void nkf_continue(unsigned char *in,int ilimit) ;
+static void nkf_end() ;
+static void none(unsigned char *a,int b);
+
+static void extend(unsigned char *out,int out_count);
+
+static FILE *nkf_file;
+
+char *
+ml_conv_decode(s,encode,charset)
+     char *s;
+     int encode,charset;
+{
+    coding_system_t coding;
+    
+    if ((s == NULL) || ((coding = CSL_FILE(ml_coding_info)) == CS_NOCONV)) 
+      return(s);
+    
+    ml_conv_sbr(s, coding, encode, charset);
+    return(s);
+}
+
+
+static void
+ml_conv_sbr(in, cs, encode, input_charset)
+     char *in;
+     coding_system_t cs;
+     int encode, input_charset;
+{
+    char *opt = cs_input_opt(cs,encode,input_charset);
+    int len = strlen(in);
+    nkf_open((unsigned char *)opt,(unsigned char *)in,len,(unsigned char *)in,len,extend,0);
+    nkf_end();
+    nkf_output[nkf_output_ctr]=0;
+}
+
+#undef CR
+#undef LIMIT
+#undef PROTO
+
+#undef getc
+#undef ungetc
+
+#define getc(f)   	nkf_getc(f)
+#define ungetc(c,f)	nkf_ungetc(c,f)
+
+static int
+nkf_getc(FILE *f) {
+    if (nkf_ilimit==-1) {
+	int c = nkf_input[nkf_input_ctr++];
+	if (c==0) {
+	    nkf_input_ctr--; return -1;
+	}
+	return c;
+    }
+    return (nkf_input_ctr>=nkf_ilimit?-1:nkf_input[nkf_input_ctr++]);
+}
+
+static int
+nkf_ungetc(int c,FILE *f) {
+    nkf_input_ctr--;
+    return c;
+}
+
+
+#undef putchar
+#undef TRUE
+#undef FALSE
+#define putchar(c)	nkf_putchar(c)
+
+#define debug nkf_debug
+
+static
+int
+nkf_putchar(unsigned int c) 
+{
+    /* string length is enough? */
+    if(nkf_output_ctr<nkf_limit && c!='\n') {
+	return nkf_output[nkf_output_ctr++] = c;
+    } else {
+	nkf_output[nkf_output_ctr++] = c;
+	nkf_flush(nkf_output,nkf_output_ctr);
+	nkf_output_ctr = 0;
+    }
+    return c;
+}
+
+
+/* Include kanji filter main part */
+/* getchar and putchar will be replaced during inclusion */
+
+#define version  NKF_version
+#define PERL_XS 1
+#include "../nkf-utf8/utf8tbl.c"
+#include "../nkf-utf8/nkf.c"
+
+/*
+      using opt ( "-w8" etc... )
+      convert *in into *out
+      when limit or nkf_end flush is called
+
+      nkf_continue can be use to change input ptr
+ */
+
+static void
+nkf_open(unsigned char *opt, unsigned char *in,int ilimit,
+	unsigned char *out,int limit,void (*flush)(unsigned char *,int),FILE *fp) {
+    /* Flags are reset at each call. */
+    reinit();
+
+    /* Process flags except the last once */
+    options(opt);
+    
+    nkf_input_ctr = 0;
+    nkf_input  = in;
+    nkf_ilimit =  ilimit;
+
+    nkf_file = fp;
+
+    nkf_output_ctr = 0;
+    nkf_output = out;
+
+    nkf_limit =  limit;
+    nkf_flush =  flush;
+
+    /* Convestion */
+    kanji_convert(NULL);
+}
+
+static void
+nkf_continue(unsigned char *in,int ilimit) {
+    nkf_input_ctr = 0;
+    nkf_input  = in;
+    nkf_ilimit =  ilimit;
+    /* Convestion */
+    kanji_convert(NULL);
+}
+
+void
+nkf_end() {
+    if (nkf_output_ctr) {
+	nkf_flush(nkf_output,nkf_output_ctr);
+	nkf_output_ctr = 0;
+    }
+}
+
+static void
+extend(unsigned char *out,int out_count)
+{
+    nkf_output += nkf_output_ctr;
+}
+
+static void
+none(unsigned char *a,int b)
+{
+}
+
+
+
+#endif /* JAPAN */
+
+/* end */
